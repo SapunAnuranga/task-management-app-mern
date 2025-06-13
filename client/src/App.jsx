33584149d91911add4b4704cf27b064702277b03
SAPUN ANURANGA <sapunanuranga98@gmail.com>
2025-06-13 18:11:56 +0530

    add dynamic sidebar navigation based on user role

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,26 @@ import Users from "./pages/Users";
 import Trash from "./pages/Trash";
 import TaskDetails from "./pages/TaskDetails";
 import { Toaster } from "sonner";
+import { useSelector } from "react-redux";
+import Sidebar from "./components/Sidebar";
+
 
 function Layout() {
-  const user = '';
+
+  const {user} = useSelector((state) => state.auth);
+
   const location = useLocation();
 
   return user ? (
     <div className='w-full h-screen flex flex-col md:flex-row'>
       <div className='w-1/5 h-screen bg-white dark:bg-[#1f1f1f] sticky top-0 hidden md:block'>
-        <Sidebar />
+          <Sidebar /> 
       </div>
 
-      <MobileSidebar />
+       { /* <MobileSidebar />  */ }
 
       <div className='flex-1 overflow-y-auto'>
-        <Navbar />
+        { /* <Navbar /> */ }
 
         <div className='p-4 2xl:px-10'>
           <Outlet />
@@ -37,7 +42,7 @@ function App() {
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route index path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/completed/:status" element={<Tasks />} />
